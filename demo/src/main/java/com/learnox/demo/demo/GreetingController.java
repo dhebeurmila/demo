3b6be555cf5e2fa05dde965a3994380008225549
dhebeurmila <dhebeurmila0@gmail.com>
2024-11-06 12:51:58 +0530

    added building restful webservice files

---
+++ demo/src/main/java/com/learnox/demo/demo/GreetingController.java
@@ -0,0 +1,27 @@
+package com.learnox.demo.demo;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import com.learnox.demo.Greeting;
+
+@RestController
+public class GreetingController {
+
+
+
+    private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+
+    @GetMapping("/greeting")
+    Greeting greeting(@RequestParam(value = "name", defaultValue = "World")  String name){
+        return new Greeting(counter.incrementAndGet(), String.format(template,name));
+    }
+
+}
